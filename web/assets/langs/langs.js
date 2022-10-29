90cb773ca6ada45c973062730f92f8b2f7683a4d
Mohammad Foroughi <foroughi1380@gmail.com>
2022-10-29 23:27:25 +0330

    update: create supoorted lanquage array

---
+++ web/assets/langs/langs.js
@@ -0,0 +1,12 @@
+supportLangs = [
+    {
+       name : "English",
+       value : "en_US",
+       icon : "🇨🇳"
+    },
+    {
+        name : "汉语",
+        value : "zh_Hans",
+        icon : "🇺🇸"
+    },
+]
