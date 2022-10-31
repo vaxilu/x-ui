d6f8c9d95af329ecfc44ea37f26c8186a0c18774
Mohammad Foroughi <foroughi1380@gmail.com>
2022-10-31 13:29:07 +0330

    update :translate base cotroller

--- web/controller/base.go
+++ web/controller/base.go
@@ -12,7 +12,7 @@ type BaseController struct {
 func (a *BaseController) checkLogin(c *gin.Context) {
 	if !session.IsLogin(c) {
 		if isAjax(c) {
-			pureJsonMsg(c, false, "登录时效已过，请重新登录")
+			pureJsonMsg(c, false, I18n(c , "pages.login.loginAgain"))
 		} else {
 			c.Redirect(http.StatusTemporaryRedirect, c.GetString("base_path"))
 		}
