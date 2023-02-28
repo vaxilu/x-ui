c38be9d1bd8f93d915d1dbdc99cd7e8abed2cc34
ojooubeh <evenfall1994@gmail.com>
2023-02-28 21:06:15 +0330

    Update inbound.go

--- web/service/inbound.go
+++ web/service/inbound.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"time"
 	"x-ui/database"
+	"encoding/json"
 	"x-ui/database/model"
 	"x-ui/util/common"
 	"x-ui/xray"
+	"x-ui/logger"
 
 	"gorm.io/gorm"
 )
@@ -17,7 +19,7 @@ type InboundService struct {
 func (s *InboundService) GetInbounds(userId int) ([]*model.Inbound, error) {
 	db := database.GetDB()
 	var inbounds []*model.Inbound
-	err := db.Model(model.Inbound{}).Where("user_id = ?", userId).Find(&inbounds).Error
+	err := db.Model(model.Inbound{}).Preload("ClientStats").Where("user_id = ?", userId).Find(&inbounds).Error
 	if err != nil && err != gorm.ErrRecordNotFound {
 		return nil, err
 	}
@@ -27,7 +29,7 @@ func (s *InboundService) GetInbounds(userId int) ([]*model.Inbound, error) {
 func (s *InboundService) GetAllInbounds() ([]*model.Inbound, error) {
 	db := database.GetDB()
 	var inbounds []*model.Inbound
-	err := db.Model(model.Inbound{}).Find(&inbounds).Error
+	err := db.Model(model.Inbound{}).Preload("ClientStats").Find(&inbounds).Error
 	if err != nil && err != gorm.ErrRecordNotFound {
 		return nil, err
 	}
@@ -48,16 +50,21 @@ func (s *InboundService) checkPortExist(port int, ignoreId int) (bool, error) {
 	return count > 0, nil
 }
 
-func (s *InboundService) AddInbound(inbound *model.Inbound) error {
+func (s *InboundService) AddInbound(inbound *model.Inbound) (*model.Inbound,error) {
 	exist, err := s.checkPortExist(inbound.Port, 0)
 	if err != nil {
-		return err
+		return inbound, err
 	}
 	if exist {
-		return common.NewError("端口已存在:", inbound.Port)
+		return inbound, common.NewError("端口已存在:", inbound.Port)
 	}
 	db := database.GetDB()
-	return db.Save(inbound).Error
+
+	err = db.Save(inbound).Error
+	if err == nil {
+		s.UpdateClientStat(inbound.Id,inbound.Settings)
+	}
+	return inbound, err
 }
 
 func (s *InboundService) AddInbounds(inbounds []*model.Inbound) error {
@@ -96,7 +103,12 @@ func (s *InboundService) DelInbound(id int) error {
 	db := database.GetDB()
 	return db.Delete(model.Inbound{}, id).Error
 }
-
+func (s *InboundService) DelInboundByPort(port int) error {
+	db := database.GetDB()
+	var inbound model.Inbound
+	db.First(&inbound, "port = ?", port)
+	return db.Delete(&inbound).Error
+}
 func (s *InboundService) GetInbound(id int) (*model.Inbound, error) {
 	db := database.GetDB()
 	inbound := &model.Inbound{}
@@ -107,18 +119,18 @@ func (s *InboundService) GetInbound(id int) (*model.Inbound, error) {
 	return inbound, nil
 }
 
-func (s *InboundService) UpdateInbound(inbound *model.Inbound) error {
+func (s *InboundService) UpdateInbound(inbound *model.Inbound) (*model.Inbound, error) {
 	exist, err := s.checkPortExist(inbound.Port, inbound.Id)
 	if err != nil {
-		return err
+		return inbound, err
 	}
 	if exist {
-		return common.NewError("端口已存在:", inbound.Port)
+		return inbound, common.NewError("端口已存在:", inbound.Port)
 	}
 
 	oldInbound, err := s.GetInbound(inbound.Id)
 	if err != nil {
-		return err
+		return inbound, err
 	}
 	oldInbound.Up = inbound.Up
 	oldInbound.Down = inbound.Down
@@ -134,8 +146,9 @@ func (s *InboundService) UpdateInbound(inbound *model.Inbound) error {
 	oldInbound.Sniffing = inbound.Sniffing
 	oldInbound.Tag = fmt.Sprintf("inbound-%v", inbound.Port)
 
+	s.UpdateClientStat(inbound.Id,inbound.Settings)
 	db := database.GetDB()
-	return db.Save(oldInbound).Error
+	return inbound, db.Save(oldInbound).Error
 }
 
 func (s *InboundService) AddTraffic(traffics []*xray.Traffic) (err error) {
@@ -165,6 +178,56 @@ func (s *InboundService) AddTraffic(traffics []*xray.Traffic) (err error) {
 	}
 	return
 }
+func (s *InboundService) AddClientTraffic(traffics []*xray.ClientTraffic) (err error) {
+	if len(traffics) == 0 {
+		return nil
+	}
+	db := database.GetDB()
+	db = db.Model(xray.ClientTraffic{})
+	tx := db.Begin()
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
+		}
+	}()
+	for _, traffic := range traffics {
+		inbound := &model.Inbound{}
+
+		err := db.Model(model.Inbound{}).Where("settings like ?", "%" + traffic.Email + "%").First(inbound).Error
+		traffic.InboundId = inbound.Id
+		if err != nil {
+			logger.Warning("AddClientTraffic find model ", err, traffic.Email)
+			continue
+		}
+		// get settings clients
+		settings := map[string][]model.Client{}
+		json.Unmarshal([]byte(inbound.Settings), &settings)
+		clients := settings["clients"]
+		for _, client := range clients {
+			if traffic.Email == client.Email {
+				traffic.ExpiryTime = client.ExpiryTime
+				traffic.Total = client.TotalGB
+			}
+		}
+		if tx.Where("inbound_id = ?", inbound.Id).Where("email = ?", traffic.Email).
+		UpdateColumn("enable", true).
+		UpdateColumn("expiry_time", traffic.ExpiryTime).
+		UpdateColumn("total",traffic.Total).
+		UpdateColumn("up", gorm.Expr("up + ?", traffic.Up)).
+		UpdateColumn("down", gorm.Expr("down + ?", traffic.Down)).RowsAffected == 0 {
+			err = tx.Create(traffic).Error
+		}
+		
+		if err != nil {
+			logger.Warning("AddClientTraffic update data ", err)
+			continue
+		}
+	
+	}
+	return
+}
 
 func (s *InboundService) DisableInvalidInbounds() (int64, error) {
 	db := database.GetDB()
@@ -175,4 +238,102 @@ func (s *InboundService) DisableInvalidInbounds() (int64, error) {
 	err := result.Error
 	count := result.RowsAffected
 	return count, err
+}
+func (s *InboundService) DisableInvalidClients() (int64, error) {
+	db := database.GetDB()
+	now := time.Now().Unix() * 1000
+	result := db.Model(xray.ClientTraffic{}).
+		Where("((total > 0 and up + down >= total) or (expiry_time > 0 and expiry_time <= ?)) and enable = ?", now, true).
+		Update("enable", false)
+	err := result.Error
+	count := result.RowsAffected
+	return count, err
+}
+func (s *InboundService) UpdateClientStat(inboundId int, inboundSettings string) (error) {
+	db := database.GetDB()
+
+	// get settings clients
+	settings := map[string][]model.Client{}
+	json.Unmarshal([]byte(inboundSettings), &settings)
+	clients := settings["clients"]
+	for _, client := range clients {
+		result := db.Model(xray.ClientTraffic{}).
+		Where("inbound_id = ? and email = ?", inboundId, client.Email).
+		Updates(map[string]interface{}{"enable": true, "total": client.TotalGB, "expiry_time": client.ExpiryTime})
+		if result.RowsAffected == 0 {
+			clientTraffic := xray.ClientTraffic{}
+			clientTraffic.InboundId = inboundId
+			clientTraffic.Email = client.Email
+			clientTraffic.Total = client.TotalGB
+			clientTraffic.ExpiryTime = client.ExpiryTime
+			clientTraffic.Enable = true
+			clientTraffic.Up = 0
+			clientTraffic.Down = 0
+			db.Create(&clientTraffic)
+		}
+		err := result.Error
+		if err != nil {
+			return err
+		}
+	
+	}
+	return nil
+}
+
+func (s *InboundService) GetInboundClientIps(clientEmail string) (string, error) {
+	db := database.GetDB()
+	InboundClientIps := &model.InboundClientIps{}
+	err := db.Model(model.InboundClientIps{}).Where("client_email = ?", clientEmail).First(InboundClientIps).Error
+	if err != nil {
+		return "", err
+	}
+	return InboundClientIps.Ips, nil
+}
+func (s *InboundService) ClearClientIps(clientEmail string) (error) {
+	db := database.GetDB()
+
+	result := db.Model(model.InboundClientIps{}).
+		Where("client_email = ?", clientEmail).
+		Update("ips", "")
+	err := result.Error
+
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (s *InboundService) ClearTrafficByPort(port int) error {
+	db := database.GetDB()
+	Uperr := db.Model(model.Inbound{}).Where("port = ?", port).Update("up", 0).Error
+	if Uperr != nil {
+		fmt.Println("ClearTrafficByPort error:clear up failed")
+		return Uperr
+	}
+	Downerr := db.Model(model.Inbound{}).Where("port = ?", port).Update("down", 0).Error
+	if Downerr != nil {
+		fmt.Println("ClearTrafficByPort error:clear down failed")
+		return Downerr
+	}
+	return nil
+}
+
+func (s *InboundService) ClearAllInboundTraffic() error {
+	inbounds, _ := s.GetAllInbounds()
+	for _, inbound := range inbounds {
+		err := s.ClearTrafficByPort(inbound.Port)
+		if err != nil {
+			fmt.Printf("ClearAllInboundTraffic error,ClearTrafficByPort port %d fail", inbound.Port)
+			continue
+		}
+	}
+	return nil
+}
+func (s *InboundService) DisableInboundByPort(port int) error {
+	db := database.GetDB()
+	return db.Model(model.Inbound{}).Where("port = ?", port).Update("enable", false).Error
+}
+func (s *InboundService) EnableInboundByPort(port int) error {
+	db := database.GetDB()
+	return db.Model(model.Inbound{}).Where("port = ?", port).Update("enable", true).Error
 }
